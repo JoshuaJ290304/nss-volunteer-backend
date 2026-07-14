15f5cb05f4fbea70b7cab35f8027a972e7649938
Joshua J <jj4440024@gmail.com>
2026-07-14 22:56:00 +0530

    Configure Express and MongoDB

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+.connect("mongodb://Joshua:qwerty123456@ac-sfucm5l-shard-00-00.adz9f2y.mongodb.net:27017,ac-sfucm5l-shard-00-01.adz9f2y.mongodb.net:27017,ac-sfucm5l-shard-00-02.adz9f2y.mongodb.net:27017/hackathondb?ssl=true&replicaSet=atlas-tq7irz-shard-0&authSource=admin&appName=Cluster0")
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.log(err));
+
+app.listen(3000, () => {
+    console.log("Server Started");
+});
